fed6c8c1e5c8596985f7e6e818c2df1ac04b8ff1
Michael Coughlin <michael.coughlin@ligo.org>
2018-03-23 19:30:47 -0700

    Add bin

---
+++ setup.sh
@@ -0,0 +1,14 @@
+
+export PATH=/home/mcoughlin/ZTF/cfitsio/bin:$PATH
+export PATH=/home/mcoughlin/ZTF/hotpants:$PATH
+export PATH=/home/mcoughlin/ZTF/swarp-2.38.0/bin:$PATH
+export PATH=/home/mcoughlin/ZTF/sextractor-2.19.5/bin:$PATH
+export PATH=/home/mcoughlin/ZTF/astrometry.net/bin:$PATH
+export PATH=/home/mcoughlin/ZTF/netpbm_install/bin:$PATH
+export PATH=/home/mcoughlin/ZTF/scamp-2.0.4/bin:$PATH
+
+export LD_LIBRARY_PATH=/home/mcoughlin/ZTF/netpbm_install/lib/:$LD_LIBRARY_PATH
+
+export ZTF_USERNAME=fremling
+export ZTF_PASSWORD=zweb4848
+
