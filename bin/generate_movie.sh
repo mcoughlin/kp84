b7d189776ceb803759408a61e280216c0fabe4e3
Michael Coughlin <coughlin@physics.harvard.edu>
2018-06-07 11:14:53 -0700

    Reductions

---
+++ bin/generate_movie.sh
@@ -0,0 +1,3 @@
+
+ffmpeg -an -y -r 20 -i plots/fits_%03d.png -b:v 5000k out.gif 
+
--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+export PATH=/Users/mcoughlin/Code/KP84/astrometry.net/bin:$PATH
 
